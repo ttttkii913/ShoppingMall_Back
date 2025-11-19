f9e8611635522b73dcffdb9383fbf5ed22b7c8fc
ttttkii913 <dbgkdud821@daum.net>
2025-11-20 02:06:41 +0900

    ♻️ Refactor/#16: CartInfoResDto productPrice 추가

--- shoppingmall/src/main/java/org/shoppingmall/cart/api/dto/response/CartInfoResDto.java
+++ shoppingmall/src/main/java/org/shoppingmall/cart/api/dto/response/CartInfoResDto.java
@@ -5,13 +5,15 @@ import org.shoppingmall.cartItem.domain.CartItem;
 public record CartInfoResDto(
         Long cartItemId,
         Long productOptionId,
-        Integer quantity
+        Integer quantity,
+        Integer price
 ) {
-public CartInfoResDto(CartItem cartItem) {
-    this(
-            cartItem.getId(),
-            cartItem.getProductOption().getId(),
-            cartItem.getQuantity()
-    );
-}
+    public CartInfoResDto(CartItem cartItem) {
+        this(
+                cartItem.getId(),
+                cartItem.getProductOption().getId(),
+                cartItem.getQuantity(),
+                cartItem.getProductOption().getProduct().getPrice()
+        );
+    }
 }
