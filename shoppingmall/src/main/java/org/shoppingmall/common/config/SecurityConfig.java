b4ff76d5cda418857160de882efe2bf1026f50e2
hayeong <139212932+ttttkii913@users.noreply.github.com>
2026-05-05 03:31:51 +0900

    Refactor/#24: Add allowed origin for production frontend

--- shoppingmall/src/main/java/org/shoppingmall/common/config/SecurityConfig.java
+++ shoppingmall/src/main/java/org/shoppingmall/common/config/SecurityConfig.java
@@ -56,7 +56,10 @@ public class SecurityConfig {
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(List.of(
+            "http://localhost:5173",
+            "https://shopping-mall-front-web.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
